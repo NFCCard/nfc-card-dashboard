e63fd7f7ed6189292a1d340691376aa66db4621c
Mohammad Kamalipour <Mohammad.kp8093@gmail.com>
2022-09-17 22:55:30 +0430

    adasdad

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./assets/fonts/font-style.css";
+import "./assets/font-awesome/css/all.min.css";
+import "./styles/global.scss";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const routes = [
+	{
+		path: "/",
+		element: <h2>home</h2>,
+		key: "dashboard",
+		name: "Dashboard",
+		icon: "fas fa-home",
+	},
+];
+
+export default routes;
