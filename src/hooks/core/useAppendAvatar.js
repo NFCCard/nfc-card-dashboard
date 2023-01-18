f05b3e2d469595e4b3c0f4484429fc4cf07a4f5c
alihoushngi <62546675+alihoushngi@users.noreply.github.com>
2023-01-18 17:26:17 +0330

    refetch user when upload avatar

--- src/hooks/core/useAppendAvatar.js
+++ src/hooks/core/useAppendAvatar.js
@@ -1,12 +1,17 @@
 import { useMutation } from "react-query";
 import postRequest from "../../api/post";
+import useGetUserList from "./useGetUserList";
 
 const useAppendAvatar = () => {
+	const { refetch } = useGetUserList();
+
 	return useMutation((value) => postRequest.appendAvatar(value), {
 		onError: (error, variables, context) => {
 			// An error happened!
 		},
-		onSuccess: (data, variables, context) => {},
+		onSuccess: (data, variables, context) => {
+			refetch();
+		},
 	});
 };
 
